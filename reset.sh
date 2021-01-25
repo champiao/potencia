01a7894d257afdbf6b820b47931b29edb5bc0467
champiao <rafael.leite.assis63@gmail.com>
2021-01-25 13:53:39 -0300

    commit 25/01/2021 Rafael Assis(Champiao)

--- reset.sh
+++ reset.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 HOST="$1"
 expect -c "
+#TESTE
 spawn telnet 192.168.$HOST
 expect \"Login:\"
 send \"Eproot\\r\"
 expect \"Password\"
 send \"adminEp\\r\"
 expect \"WAP>\"
-send \"display optic\\r\"
-expect \"WAP>\"
-send \"ping uol.com.br\\r\"
+send \"reset\\r\"
 expect eof
 "
