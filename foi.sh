0de44ef84fa3f4783a0f72f9950630e0304bd805
champiao <rafael.leite.assis63@gmail.com>
2021-03-03 13:24:27 -0300

    new script feature that's return just the line that contain the power Level of Huawei terminal

---
+++ foi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+IP='$1'
+/home/rassis/.potHW.sh $1 | tee /tmp/potencia.txt
+cat /tmp/potencia.txt | grep RxPower > /tmp/results.txt
+sed -i 2d /tmp/results.txt
+xclip -sel clip /tmp/results.txt
+rm -rf /tmp/potencia.txt /tmp/results.txt
