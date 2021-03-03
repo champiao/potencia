10f353854077279aac0a3b3eb920f7570361f53b
champiao <rafael.leite.assis63@gmail.com>
2021-03-03 16:48:18 -0300

    update script

--- somente potencia8120.sh
+++ somente potencia8120.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IP='$1'
-/home/rassis/.potHW8120.sh $1 | tee /tmp/potencia.txt
+~/.potHW8120.sh $1 | tee /tmp/potencia.txt
 cat /tmp/potencia.txt | grep RxPower > /tmp/results.txt
 sed -i 2d /tmp/results.txt
 xclip -sel clip /tmp/results.txt
