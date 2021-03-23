1974188496d801b951c3bdef9cc8a6c70b45b5e4
champiao <39089594+champiao@users.noreply.github.com>
2021-03-23 08:53:35 -0300

    Delete foi8120.sh

--- foi8120.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-IP='$1'
-/home/rassis/.potHW8120.sh $1 | tee /tmp/potencia.txt
-cat /tmp/potencia.txt | grep RxPower > /tmp/results.txt
-sed -i 2d /tmp/results.txt
-xclip -sel clip /tmp/results.txt
-rm -rf /tmp/potencia.txt /tmp/results.txt
